7b210427509146fb321edbb847e230d1027c9f3a
Raghav Vashisht <raghav2011rv@gmail.com>
2017-05-13 11:15:33 +0530

    Final App

--- ScrollView/ViewController.swift
+++ ScrollView/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var images = [UIImageView]();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+                }
+        
+
+        
+        override func viewDidAppear(_ animated: Bool) {
+            
+            let scrollWidth = scrollView.frame.size.width
+            var contentWidth: CGFloat = 0.0
+            for x in 0...2 {
+                let image = UIImage(named: "icon\(x).png")
+                let imageView = UIImageView(image: image)
+                images.append(imageView)
+                
+                var newX: CGFloat
+                newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
+                contentWidth += newX
+                
+                scrollView.addSubview(imageView)
+                imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height/2) - 75, width: 150, height: 150)
+                
+
+        }
+        scrollView.clipsToBounds = false
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    
+    
 }
 
